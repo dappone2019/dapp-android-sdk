155ae68de01fc5657b19927f3894d59a91fd6d76
shiwangqiang <dappone2019@hotmail.com>
2019-05-08 23:34:57 +0800

    update

--- app/src/main/java/com/blockchain/dappbirds/dappbirdsdemo/MainActivity.java
+++ app/src/main/java/com/blockchain/dappbirds/dappbirdsdemo/MainActivity.java
@@ -2,12 +2,10 @@ package com.blockchain.dappbirds.dappbirdsdemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.blockchain.dappbirds.opensdk.DappBirdsSdk;
 import com.blockchain.dappbirds.opensdk.wallet.DBWalletManager;
 
 import butterknife.BindView;
