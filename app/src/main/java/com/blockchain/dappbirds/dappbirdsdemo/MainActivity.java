5956a7a28c42a363360a476771bdae4746d269c9
shiwangqiang <dappone2019@hotmail.com>
2019-05-08 23:27:15 +0800

    update

--- app/src/main/java/com/blockchain/dappbirds/dappbirdsdemo/MainActivity.java
+++ app/src/main/java/com/blockchain/dappbirds/dappbirdsdemo/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.blockchain.dappbirds.opensdk.DappBirdsSdk;
 import com.blockchain.dappbirds.opensdk.wallet.DBWalletManager;
 
 import butterknife.BindView;
--- app/src/main/java/com/blockchain/dappbirds/dappbirdsdemo/MyApplication.java
+++ app/src/main/java/com/blockchain/dappbirds/dappbirdsdemo/MyApplication.java
@@ -14,7 +14,27 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        initSDK();
+    }
+
+    /**
+     * 初始化SDK
+     */
+    public void initSDK() {
+        /**
+         * @Boolean: 切换主网络和测试网
+         */
         DappBirdsSdk.setIs_debug(true);
-        dbWalletManager = DappBirdsSdk.create("1", "5X9qHSUPQgrZQ84mD188sd9kD3WkQN8vx", "7", this).getDbWalletManager();
+        /**
+         * @app_id: 平台分配的app_id
+         * @openid: 平台分配的openid
+         * @chain_type: 公链的类型
+         * @context : Context
+         */
+        DappBirdsSdk dappBirdsSdk = DappBirdsSdk.create("1", "5X9qHSUPQgrZQ84mD188sd9kD3WkQN8vx", "7", this);
+        /**
+         * 钱包管理类
+         */
+        dbWalletManager = dappBirdsSdk.getDbWalletManager();
     }
 }
