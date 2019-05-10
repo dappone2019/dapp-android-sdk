555264d5414e21c367d282c2bee10ad694ee970f
shiwangqiang <dappone2019@hotmail.com>
2019-05-10 14:09:45 +0800

    update

--- app/src/main/java/com/blockchain/dappbirds/dappbirdsdemo/MyApplication.java
+++ app/src/main/java/com/blockchain/dappbirds/dappbirdsdemo/MyApplication.java
@@ -1,21 +1,58 @@
 package com.blockchain.dappbirds.dappbirdsdemo;
 
 
-import android.app.Activity;
 import android.app.Application;
-import android.content.Context;
 
 import com.blockchain.dappbirds.opensdk.DappBirdsSdk;
 import com.blockchain.dappbirds.opensdk.wallet.DBWalletManager;
 
 public class MyApplication extends Application {
 
+    public static DappBirdsSdk dappBirdsSdk;
     public static DBWalletManager dbWalletManager;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        /**
+         * SDK初始化
+         */
         initSDK();
+        /**
+         * 设置数据
+         */
+        setInitData();
+        /**
+         * 得到钱包管理类
+         */
+        getDBWallet();
+    }
+
+    private void getDBWallet() {
+        dbWalletManager = dappBirdsSdk.getDbWalletManager();
+    }
+
+    private void setInitData() {
+        /**
+         * 设置appId
+         */
+        dappBirdsSdk.setAppId("1");
+        /**
+         * 设置openID
+         */
+        dappBirdsSdk.setOpenId("5X9qHSUPQgrZQ84mD188sd9kD3WkQN8vx");
+        /**
+         * 设置公链类型，目前仅支持本体 7
+         */
+        dappBirdsSdk.setChainType("7");
+        /**
+         * 是否打印日志
+         */
+        dappBirdsSdk.setEnableLog(true);
+        /**
+         * 是否是测试网络
+         */
+        dappBirdsSdk.setEnableTest(true);
     }
 
     /**
@@ -23,24 +60,9 @@ public class MyApplication extends Application {
      */
     public void initSDK() {
         /**
-         * @Boolean: 切换主网络和测试网
-         */
-        DappBirdsSdk.setIs_debug(true);
-        /**
-         * 是否打印日志
-         */
-        DappBirdsSdk.setEnableLog(true);
-        /**
-         * @app_id: 平台分配的app_id
-         * @openid: 平台分配的openid
-         * @chain_type: 公链的类型
          * @context : Context
          */
-        DappBirdsSdk dappBirdsSdk = DappBirdsSdk.create("1", "5X9qHSUPQgrZQ84mD188sd9kD3WkQN8vx", "7", this);
-        /**
-         * 钱包管理类
-         */
-        dbWalletManager = dappBirdsSdk.getDbWalletManager();
+        dappBirdsSdk = DappBirdsSdk.initSDK(this);
     }
 
     public static void exit() {
